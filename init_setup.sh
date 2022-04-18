acaad80f221582edaf183ce94795bb7834d831f9
Supreeth Shetty <shetty.supreeth95@gmail.com>
2022-04-18 16:50:45 +0530

    commit changes

--- init_setup.sh
+++ init_setup.sh
@@ -2,7 +2,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
 conda create --prefix ./env python=3.7 -y
 echo [$(date)]: "activate environment"
-source activate ./env
+conda activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "export conda environment"
