28d089dd9603c5da001a2bdf1dc66363f313dc93
Supreeth Shetty <shetty.supreeth95@gmail.com>
2022-05-03 09:48:36 +0530

    Stage 2 base model creation file added

---
+++ src/stage_02_base_model_creation.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+import tensorflow
+
+STAGE = "Base Model Creation Stage" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    
+    LAYERS = [
+    tf.keras.layers.Input(shape=(180,180,3)),
+    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(8, activation="relu"),
+    tf.keras.layers.Dense(2, activation="softmax")
+    ]
+
+    classifier = tf.keras.Sequential(LAYERS)
+
+    classifier.summary()
+
+    classifier.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+    )
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
