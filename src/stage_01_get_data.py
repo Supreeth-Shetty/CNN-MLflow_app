23dac6807dc4aabc7034307f9f9227e38043feae
Supreeth Shetty <shetty.supreeth95@gmail.com>
2022-05-04 17:00:05 +0530

    minor changes made

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import shutil
-from tqdm import tqdm
+# from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories, unzip_file
 from src.utils.data_validating import validating_img
